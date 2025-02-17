9f28c29a82f0ee3721ca3a131a44804965127b3a
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2025-02-17 20:56:46 +0200

    Update evaluate.py

--- lib/core/evaluate.py
+++ lib/core/evaluate.py
@@ -48,7 +48,7 @@ class Evaluator():
         self.evaluation_accumulators = dict.fromkeys(['pred_j3d', 'target_j3d', 'target_theta', 'pred_verts'])
 
         if self.device is None:
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            self.device = 'cpu'
 
     def validate(self):
         self.model.eval()
