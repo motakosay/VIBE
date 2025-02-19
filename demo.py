75bff8a45de092018fb02765e69a80453e8bc571
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2025-02-20 01:58:15 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -113,6 +113,7 @@ def main(args):
 
     # ========= Load pretrained weights ========= #
     pretrained_file = download_ckpt(use_3dpw=False)
+    torch.serialization.add_safe_globals([np.core.multiarray.scalar])
     ckpt = torch.load(pretrained_file, map_location=torch.device('cpu'), weights_only=True)
     print(f'Performance of pretrained model on 3DPW: {ckpt["performance"]}')
     ckpt = ckpt['gen_state_dict']
