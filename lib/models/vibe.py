430e99e6c64a7a1d6c9e7dd2ebb016b8781325ca
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2025-02-20 01:44:35 +0200

    Update vibe.py

--- lib/models/vibe.py
+++ lib/models/vibe.py
@@ -93,7 +93,7 @@ class VIBE(nn.Module):
         self.regressor = Regressor()
 
         if pretrained and os.path.isfile(pretrained):
-            pretrained_dict = torch.load(pretrained, map_location=torch.device('cpu'))['model']
+            pretrained_dict = torch.load(pretrained, map_location=torch.device('cpu'), weights_only=True)['model']
 
             self.regressor.load_state_dict(pretrained_dict, strict=False)
             print(f'=> loaded pretrained model from \'{pretrained}\'')
@@ -151,7 +151,7 @@ class VIBE_Demo(nn.Module):
         self.regressor = Regressor()
 
         if pretrained and os.path.isfile(pretrained):
-            pretrained_dict = torch.load(pretrained, map_location=torch.device('cpu'))['model']
+            pretrained_dict = torch.load(pretrained, map_location=torch.device('cpu'), weights_only=True)['model']
 
             self.regressor.load_state_dict(pretrained_dict, strict=False)
             print(f'=> loaded pretrained model from \'{pretrained}\'')
